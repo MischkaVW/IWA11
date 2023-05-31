ec56fe5933b08a1fbfb7c21103dd500ad9fb4701
Mischka van Wyk <m.vanwyk@lab.clicklearning.org>
2023-05-31 18:51:11 +0200

    changes made to IWA11

--- scriptcopy.js
+++ scriptcopy.js
@@ -1,20 +1,22 @@
+// script.js
+
 const order1 = document.querySelector('[data-key="order1"]')
 const biscuits1 = document.querySelector('[data-biscuits="10"]')
 const donuts1 = document.querySelector('[data-donuts="13"]')
 const pancakes1 = document.querySelector('[data-pancakes="0"]')
-const status1 = document.querySelector('[data-delivered="false"]')
+const status1 = document.querySelector('[status]')
 
 const order2 = document.querySelector('[data-key="order2"]')
 const biscuits2 = document.querySelector('[data-biscuits="5"]')
 const donuts2 = document.querySelector('[data-donuts="0"]')
 const pancakes2 = document.querySelector('[data-pancakes="2"]')
-const status2 = document.querySelector('[data-delivered="false"]')
+const status2 = document.querySelector('[status]')
 
 const order3 = document.querySelector('[data-key="order3]')
 const biscuits3 = document.querySelector('[data-biscuits="12"]')
 const donuts3 = document.querySelector('[data-donuts="11"]')
 const pancakes3 = document.querySelector('[data-pancakes="15"]')
-const status3 = document.querySelector('[data-delivered="true"]')
+const status3 = document.querySelector('[status]')
 
 biscuits1 = order1.biscuits,
 donuts1 = order1.donuts,
